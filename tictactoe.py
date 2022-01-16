f60380930369f62c3bb73cd2c1f737bc5d9ef87a
Daniel Uboa <uboadaniel@gmail.com>
2022-01-16 17:51:31 +0300

    New changes

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,13 @@
 import pygame, sys
+import numpy as np
 
 pygame.init()
 
 WIDTH = 600
 HEIGHT = 600
 LINE_WIDTH = 15
+BOARD_ROWS = 3
+BOARD_COLS = 3
 
 RED = (255, 0, 0)
 BG_COLOR = (28, 170, 156)
@@ -13,6 +16,10 @@ LINE_COLOR = (23, 145, 135)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC TAC TOE')
 screen.fill(BG_COLOR)
+
+# Board  
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+#print(board)
 
 #pygame.draw.line(screen, RED, (10, 10), (300, 300), 10)
 
@@ -25,6 +32,27 @@ def draw_Lines():
     pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
     # 2nd vertical line
     pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
+def mark_square(row, col, player):
+    board[row][col] = player
+
+def available_square(row, col):
+    return board[row][col] == 0
+
+def is_board_full():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 0:
+                return False
+    return True
+
+print(is_board_full())
+
+for row in range(BOARD_ROWS):
+    for col in range(BOARD_COLS):
+        mark_square(row, col, 1)
+
+print(is_board_full())
 
 draw_Lines()
 #Main loop
